084449b69955c741cd96fdd8c4ff5f44c6baeec4
Maxence Comptdaer <comptdaer.maxence23@gmail.com>
2022-11-16 15:11:34 +0100

    restaurants et reviews

---
+++ db/migrate/20221116105358_rename_adress.rb
@@ -0,0 +1,5 @@
+class RenameAdress < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :restaurants, :adress, :address
+  end
+end
---
+++ db/migrate/20221116112157_remove_restaurant_to_reviews.rb
@@ -0,0 +1,5 @@
+class RemoveRestaurantToReviews < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :reviews, :restaurant_id
+  end
+end
